3ac3854ff53870ad52bf186685d903450809f163
marcoamolinaro <molinaromarcoaurelio@gmail.com>
2022-03-27 14:58:34 -0300

    very mock tests

---
+++ unit-testing/src/test/java/com/scm/unittesting/unittesting/business/MockListTest.java
@@ -0,0 +1,63 @@
+package com.scm.unittesting.unittesting.business;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.anyInt;
+import static org.mockito.Mockito.atLeast;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.atMost;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+
+class MockListTest {
+
+	List<String> mock = mock(List.class);
+
+	@Test
+	void size_basic() {
+		when(mock.size()).thenReturn(5);
+		assertEquals(5, mock.size());
+	}
+
+	@Test
+	void returnDifferentValues() {
+		when(mock.size()).thenReturn(5).thenReturn(10);
+		assertEquals(5, mock.size());
+		assertEquals(10, mock.size());
+	}
+	
+	@Test
+	void returnWithParameters() {
+		when(mock.get(0)).thenReturn("in28Minutes");
+		assertEquals("in28Minutes", mock.get(0));
+		assertEquals(null, mock.get(1));
+	}
+
+	@Test
+	void returnWithGenericParameters() {
+		when(mock.get(anyInt())).thenReturn("in28Minutes");
+		assertEquals("in28Minutes", mock.get(0));
+		assertEquals("in28Minutes", mock.get(1));
+	}
+	
+	@Test
+	void verificationBasics() {
+		String value1 = mock.get(0);
+		String value2 = mock.get(1);
+		
+		verify(mock).get(0);
+		verify(mock, times(2)).get(anyInt());
+		verify(mock, atLeast(1)).get(anyInt());
+		verify(mock, atLeastOnce()).get(anyInt());
+		verify(mock, atMost(2)).get(anyInt());
+		verify(mock, never()).get(2);
+
+	}
+}
--- unit-testing/src/test/java/com/scm/unittesting/unittesting/business/SomeBusinessMockTest.java
+++ unit-testing/src/test/java/com/scm/unittesting/unittesting/business/SomeBusinessMockTest.java
@@ -1,7 +1,6 @@
 package com.scm.unittesting.unittesting.business;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
