02c79583013ec47b2766d4a3af7fb1c2b090f7fb
marcoamolinaro <molinaromarcoaurelio@gmail.com>
2022-03-27 11:37:21 -0300

    Mock with @ExtendWith

--- unit-testing/src/test/java/com/scm/unittesting/unittesting/business/SomeBusinessMockTest.java
+++ unit-testing/src/test/java/com/scm/unittesting/unittesting/business/SomeBusinessMockTest.java
@@ -4,22 +4,23 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.test.context.event.annotation.BeforeTestClass;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.scm.unittesting.unittesting.data.SomeDataService;
 
+@ExtendWith(MockitoExtension.class)
 class SomeBusinessMockTest {
 
-	SomeBusinessimpl business = new SomeBusinessimpl();
-	SomeDataService dataServiceMock = mock(SomeDataService.class);
+	@InjectMocks
+	SomeBusinessimpl business;
 	
-	@BeforeEach
-	public void before() {
-		business.setSomeDataService(dataServiceMock);
-	}
-
+	@Mock
+	SomeDataService dataServiceMock;
+	
 	@Test
 	void calculateSumUsingSomeDataService_basic() {
 		
